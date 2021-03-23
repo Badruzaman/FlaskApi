42b1d26f3ce75f0782a223d9d4d04bb96e1fd6b9
Badruzzaman <badru.cse@gmail.com>
2021-03-23 14:03:56 +0600

    new sql alchamey added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from flask import  Flask,jsonify,request
 from flask_cors import CORS
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
@@ -13,8 +15,13 @@ def DummyApi():
     list = [{'Name': 'Badruzzaman'}, {'Name': 'Rahim'}]
     return jsonify(list),200
 
-@app.route("/api/v1/Create/", methods=['POST'])
-def Create():
+@app.route("/api/v1/Register/", methods=['POST'])
+def Registration():
+    FIRSTNAME = request.json('FIRSTNAME')
+    LASTNAME = request.json('LASTNAME')
+    EMAIL = request.json('EMAIL')
+    PHONE = request.json('PHONE')
+    firstName = request.json('firstName')
     conn.execute("INSERT INTO Users (ID,FIRSTNAME,LASTNAME,EMAIL,PHONE) \
           VALUES (1, 'BADRU', 'ZAMAN', 'badru.cse@gmail.com', '+8801719730475')");
     conn.commit()
