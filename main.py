5f8dce09b2d577adf7aac9e3eb86bb5503afceb8
Badruzzaman <badru.cse@gmail.com>
2021-03-26 11:03:41 +0600

    df

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ def DummyApi():
     list = [{'Name': 'Badruzzaman'}, {'Name': 'Rahim'}]
     return jsonify(list),200
 
-@app.route("/api/v1/Create/", methods=['POST'])
-def Create():
-    conn.execute("INSERT INTO Users (ID,FIRSTNAME,LASTNAME,EMAIL,PHONE) \
-          VALUES (1, 'BADRU', 'ZAMAN', 'badru.cse@gmail.com', '+8801719730475')");
-    conn.commit()
-    return jsonify(list),200
+@app.route("/api/v1/Create/<string:firstname>", methods=['POST'])
+def Create(firstname):
+    #firstName = request['firstname']
+   #conn.execute("INSERT INTO Users (ID,FIRSTNAME,LASTNAME,EMAIL,PHONE) \
+          #VALUES (1, 'BADRU', 'ZAMAN', 'badru.cse@gmail.com', '+8801719730475')");
+    #conn.commit()
+    return jsonify(name = firstname),200
 
 if __name__ == "__main__":
     app.run()
